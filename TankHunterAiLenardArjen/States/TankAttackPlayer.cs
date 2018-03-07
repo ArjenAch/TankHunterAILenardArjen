5c9ad0f0e1258611b61864ca7280a65f367b227d
b93rn <lenardborn@gmail.com>
2018-03-07 21:14:50 +0100

    Tank state machine implementation

---
+++ TankHunterAiLenardArjen/States/TankAttackPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankHunterAiLenardArjen.States
+{
+    public class TankAttackPlayer : TankState
+    {
+        public void Enter(Tank tank)
+        {
+            // IDEA: tank shows signs of being mad
+        }
+
+        public void Execute(Tank tank)
+        {
+            if (tank.PlayerInSearchZone())
+            {
+                tank.ChangeState(new TankSearchForPlayer());
+            }
+            else if (tank.PlayerInDangerZone())
+            {
+                tank.ChangeState(new TankCreateDistanceBetweenPlayer());
+            }
+            else
+            {
+                // tank.AttackPlayer();
+            }
+        }
+
+        public void Exit(Tank tank)
+        {
+            
+        }
+    }
+}
---
+++ TankHunterAiLenardArjen/States/TankCreateDistanceBetweenPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankHunterAiLenardArjen.States
+{
+    public class TankCreateDistanceBetweenPlayer : TankState
+    {
+        public void Enter(Tank tank)
+        {
+            // IDEA: tank shows signs of panic
+        }
+
+        public void Execute(Tank tank)
+        {
+            if (tank.PlayerInAttackZone())
+            {
+                tank.ChangeState(new TankAttackPlayer());
+            }
+            else
+            {
+                // tank.Flee();
+            }
+        }
+
+        public void Exit(Tank tank)
+        {
+
+        }
+    }
+}
