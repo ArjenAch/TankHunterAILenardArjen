9f24ea7d7160fd8728a8b13bb790d5e10ddaf796
b93rn <lenardborn@gmail.com>
2018-03-12 15:43:25 +0100

    Empty input for player class

---
+++ TankHunterAiLenardArjen/PlayerInput/InputController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankHunterAiLenardArjen.PlayerInput
+{
+    public class InputController
+    {
+        Player player;
+
+        public InputController(Player player)
+        {
+            
+        }
+    }
+}
